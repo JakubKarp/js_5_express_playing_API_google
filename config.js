4734d0ba9a07494994d36a46c1e07eef65dbc4ed
JakubKarp <jakub.karp@innespacery.pl>
2018-01-15 17:25:00 +0100

    playing api

---
+++ config.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+	'GOOGLE_CLIENT_ID': '511362918841-a200umv0sa6cob4ocobbb2q80c1vp5ld.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'xwXv5qtHNHh1mu-Q9TB2b3b0',
+	'CALLBACK_URL': 'http://localhost:3000/auth/googlecallback'
+}
